f0fdf1764fd041371410115fc8839e85ef1693f6
AlexBrodbelt <apbrodbelt@gmail.com>
2022-12-04 19:09:25 +0000

    generating starting position for red balls

---
+++ src/red_ball_positions.py
@@ -0,0 +1,22 @@
+
+import math
+
+def get_red_ball_at(screen_width, screen_height, index, level, radius, GAP):
+    x = 3/4 * screen_width + math.sqrt(3)/2*(radius+ GAP)*level
+    y = 1/2 * screen_height + (2*index - level) * (radius + GAP)
+    return x, y
+
+def get_red_ball_positions(screen_width, screen_height, radius, GAP):
+    positions = []
+    for level in range(1,6):
+        for index in range(level):
+            positions.append(get_red_ball_at(screen_width, screen_height, level, index, radius, GAP))
+    return positions
+
+if __name__ == "__main__":
+    GAP = 1
+    screen_height = 180
+    screen_width = 360
+    radius  = 5
+    red_balls = get_red_ball_positions(screen_width, screen_height, radius, GAP)
+    print(red_balls)
--- src/snooker.py
+++ src/snooker.py
@@ -1,16 +1,20 @@
-import os, sys, math, pygame, pygame.mixer
-import random
-import euclid
-from pygame.locals import *
+import os, sys, math, euclid, random, pygame
+from red_ball_positions import get_red_ball_positions
+#import pygame.mixer
+#from pygame.locals import *
 
 pygame.init()
 
+# Scale up/down of snooker table
 SCALE = 3
+
+# Real snooker table dimensions
 SNOOKER_TABLE_WIDTH = 360
 SNOOKER_TABLE_HEIGHT = 180
 
 screen_size = screen_width, screen_height = SCALE*SNOOKER_TABLE_WIDTH, SCALE*SNOOKER_TABLE_HEIGHT
 
+# Color Palette
 BLACK = 0, 0, 0
 WHITE = 255, 255, 255
 YELLOW = 255, 174, 66
@@ -21,6 +25,7 @@ PINK = 255, 20, 147
 BROWN = 139, 69, 19
 BACKGROUND_GREEN = 10, 108, 3
 
+# Snooker table features
 X_BAULK_LINE = screen_width / 5 # x coordinate for Baulk Line
 R_BAULK_D = screen_height / 6 # radius of Baulk D
 X_BAULK_D = X_BAULK_LINE - R_BAULK_D
@@ -29,7 +34,8 @@ Y_BAULK_D = screen_height / 3
 
 initial_velocity = 70
 MAX_VELOCITY = 1000
-BALL_SIZE = 21
+BALL_SIZE = SCALE*3
+GAP = BALL_SIZE / 5
 
 colors = [BLACK, RED, GREEN, BLUE]
 
@@ -46,7 +52,7 @@ colors = [BLACK, RED, GREEN, BLUE]
         pygame.draw.rect(screen, self.color, self.rectangle,width = 1)"""
 
 class Ball:
-    def __init__(self, radius, color, position=euclid.Vector2(0,0), velocity=euclid.Vector2(0,0), width=1):
+    def __init__(self, radius, color, position=euclid.Vector2(0,0), velocity=euclid.Vector2(0,0), width=0):
         self.position = position
         self.velocity = velocity
         self.radius = radius
@@ -118,7 +124,7 @@ class Ball:
             else:
                 self.collide(other)
 
-"""Intermediate step for generating random setup"""
+"""Intermediate step - generating random setup"""
 def get_random_position(size):
     x = random.randint(size,screen_width-size)
     y = random.randint(size,screen_height-size)
@@ -163,25 +169,33 @@ def setUpBackground():
     pygame.draw.arc(screen,WHITE,(X_BAULK_D, Y_BAULK_D, 2*R_BAULK_D, 2*R_BAULK_D), 1/2 * math.pi, 3/2 * math.pi)
     return
 
-def setUpBalls():
-    green_ball  = Ball(BALL_SIZE, GREEN, euclid.Vector2( X_BAULK_D, Y_BAULK_D))
-    brown_ball  = Ball(BALL_SIZE, BROWN, euclid.Vector2( X_BAULK_D, screen_height/2))
-    yellow_ball = Ball(BALL_SIZE, YELLOW, euclid.Vector2( X_BAULK_D, 2 * Y_BAULK_D))
-    blue_ball   = Ball(BALL_SIZE, BLUE, euclid.Vector2( 1/2 * screen_width, screen_height / 2 ))
-    pink_ball   = Ball(BALL_SIZE, PINK, euclid.Vector2( 3/4 * screen_width, screen_height / 2 ))
-    black_ball  = Ball(BALL_SIZE, BLACK, euclid.Vector2( 7/8 * screen_width, screen_height / 2 ))
-    
-    
+def reset():
+    pass
+
+"""BALL SETUP"""
+# non-red balls
+green_ball  = Ball(BALL_SIZE, GREEN, euclid.Vector2( X_BAULK_LINE, Y_BAULK_D))
+brown_ball  = Ball(BALL_SIZE, BROWN, euclid.Vector2( X_BAULK_LINE, screen_height/2))
+yellow_ball = Ball(BALL_SIZE, YELLOW, euclid.Vector2( X_BAULK_LINE, 2 * Y_BAULK_D))
+blue_ball   = Ball(BALL_SIZE, BLUE, euclid.Vector2( 1/2 * screen_width, screen_height / 2 ))
+pink_ball   = Ball(BALL_SIZE, PINK, euclid.Vector2( 3/4 * screen_width, screen_height / 2 ))
+black_ball  = Ball(BALL_SIZE, BLACK, euclid.Vector2( 7/8 * screen_width, screen_height / 2 ))
+# red balls
+red_balls = [Ball(BALL_SIZE, RED, euclid.Vector2(x,y)) for x,y in get_red_ball_positions(screen_width, screen_height, BALL_SIZE, GAP)] 
+# creating a list containing all balls 
+balls = [green_ball, brown_ball, yellow_ball, blue_ball, black_ball, pink_ball, black_ball]
+balls.extend(red_balls)
 
 
-for i in range(num_of_balls):
+
+"""for i in range(num_of_balls):
     color = random.choice(colors)
     width = 1
     size = random.randint(10,40)
     position = get_random_position(size)
     velocity = get_random_velocity()
     ball = Ball(size,color,position,velocity)
-    balls.append(ball)
+    balls.append(ball)"""
 
 direction_tick = 0
 
