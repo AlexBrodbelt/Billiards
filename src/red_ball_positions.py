2491635b5cb55da0de81c7e2c0881efba674824a
AlexBrodbelt <apbrodbelt@gmail.com>
2022-12-25 19:43:49 +0000

    added id to balls + changed from euclid to pygame.math vector library

--- src/red_ball_positions.py
+++ src/red_ball_positions.py
@@ -4,14 +4,14 @@ import math
 def get_red_ball_at(screen_width, screen_height, index, level, radius, GAP):
     x_offset = 3/4 * screen_width
     y_offset = 1/2 * screen_height
-    x = x_offset + math.sqrt(3)*(radius + GAP)*(level+1)
+    x = x_offset + math.sqrt(3)*(radius + GAP)*(level+1) # level: from left to right ; index: from down to up
     y = y_offset + (2*index - level) * (radius + GAP)
     return x, y
 
 def get_red_ball_positions(screen_width, screen_height, radius, GAP):
     positions = []
     for level in range(5):
-        for index in range(level+1):
+        for index in range(level + 1):
             positions.append(get_red_ball_at(screen_width, screen_height, index, level, radius, GAP))
     return positions
 
--- src/snooker.py
+++ src/snooker.py
@@ -1,4 +1,4 @@
-import os, sys, math, euclid, random, pygame
+import os, sys, math, random, pygame
 from red_ball_positions import get_red_ball_positions
 #import pygame.mixer
 #from pygame.locals import *
@@ -13,6 +13,8 @@ SNOOKER_TABLE_WIDTH = 360
 SNOOKER_TABLE_HEIGHT = 180
 
 screen_size = screen_width, screen_height = SCALE*SNOOKER_TABLE_WIDTH, SCALE*SNOOKER_TABLE_HEIGHT
+
+id = 0
 
 # Color Palette
 BLACK = 0, 0, 0
@@ -52,13 +54,19 @@ colors = [BLACK, RED, GREEN, BLUE]
         pygame.draw.rect(screen, self.color, self.rectangle,width = 1)"""
 
 class Ball:
-    def __init__(self, radius, color, position=euclid.Vector2(0,0), velocity=euclid.Vector2(0,0), width=0):
+    def __init__(self, radius, color, position=pygame.math.Vector2(0,0), velocity=pygame.math.Vector2(0,0), width=0):
         self.position = position
         self.velocity = velocity
         self.radius = radius
         self.color = color
         self.width = width
         self.mass = math.pi*self.radius**2
+        global id
+        self.id = id
+        id += 1
+        
+    
+        
 
     def move(self):
         self.position += self.velocity * dtime_s
@@ -81,7 +89,7 @@ class Ball:
     def distance_to_other(self, other):
         posA = self.position + self.velocity*dtime_s
         posB = other.position + other.velocity*dtime_s
-        distance = abs(posA - posB)
+        distance = (posA - posB).magnitude()
         sumOfRadii = self.radius + other.radius
         return distance - sumOfRadii
 
@@ -91,9 +99,33 @@ class Ball:
         self.velocity = self.velocity.reflect(collision_vector)
         other.velocity = other.velocity.reflect(collision_vector)
 
+    def velocities_after_collision(self, ball):
+        normal = self.position - ball.position
+        if normal.magnitude() == 0:
+            print("ball in the same position", self.id, ball.id)
+        normal.normalize()
+        tangent = pygame.math.Vector2(-normal.y, normal.x)
+        # tangent components of velocity before and after collision
+        v1_tangent = self.velocity.project(tangent)
+        v2_tangent = ball.velocity.project(tangent)
+        # initial normal components of the velocity
+        v1_normal_scalar_0 = self.velocity.dot(normal)
+        v2_normal_scalar_0 = self.velocity.dot(normal)  
+        combined_mass = ball.mass + self.mass # combined mass of both balls
+        # normal components of the velocity after the elastic collision
+        v1_normal_scalar = (v1_normal_scalar_0*(self.mass - ball.mass) + 2*ball.mass*v2_normal_scalar_0) / combined_mass
+        v2_normal_scalar = (v2_normal_scalar_0*(ball.mass - self.mass) + 2*self.mass*v1_normal_scalar_0) / combined_mass
+        # normal components after collision
+        v1_normal = v1_normal_scalar * normal
+        v2_normal = v2_normal_scalar * normal
+
+        # update velocities
+        self.change_velocity(v1_tangent + v1_normal)
+        ball.change_velocity(v2_tangent + v2_normal)
+
     def collision_with_ball(self, other):
         if self.distance_to_other(other) <= 0:
-            self.collide(other)
+            self.velocities_after_collision(other)
 
 
 # Setting screen size
@@ -116,7 +148,7 @@ def set_up_background():
     # Baulk Line
     pygame.draw.line(screen, WHITE, (X_BAULK_LINE, 0), (X_BAULK_LINE, screen_height))
     # Baulk D
-    pygame.draw.arc(screen,WHITE,(X_BAULK_D, Y_BAULK_D, 2*R_BAULK_D, 2*R_BAULK_D), 1/2 * math.pi, 3/2 * math.pi)
+    pygame.draw.arc(screen, WHITE, (X_BAULK_D, Y_BAULK_D, 2*R_BAULK_D, 2*R_BAULK_D), 1/2 * math.pi, 3/2 * math.pi)
     return
 
 def reset_poisitions():
@@ -124,19 +156,20 @@ def reset_poisitions():
 
 """BALL SETUP"""
 # non-red balls
-green_ball  = Ball(BALL_SIZE, GREEN, euclid.Vector2( X_BAULK_LINE, Y_BAULK_D))
-brown_ball  = Ball(BALL_SIZE, BROWN, euclid.Vector2( X_BAULK_LINE, screen_height/2))
-yellow_ball = Ball(BALL_SIZE, YELLOW, euclid.Vector2( X_BAULK_LINE, 2 * Y_BAULK_D))
-blue_ball   = Ball(BALL_SIZE, BLUE, euclid.Vector2( 1/2 * screen_width, screen_height / 2 ))
-pink_ball   = Ball(BALL_SIZE, PINK, euclid.Vector2( 3/4 * screen_width, screen_height / 2 ))
-black_ball  = Ball(BALL_SIZE, BLACK, euclid.Vector2( 7/8 * screen_width, screen_height / 2 ))
+green_ball  = Ball(BALL_SIZE, GREEN, pygame.math.Vector2( X_BAULK_LINE, Y_BAULK_D))
+brown_ball  = Ball(BALL_SIZE, BROWN, pygame.math.Vector2( X_BAULK_LINE, screen_height/2))
+yellow_ball = Ball(BALL_SIZE, YELLOW, pygame.math.Vector2( X_BAULK_LINE, 2 * Y_BAULK_D))
+blue_ball   = Ball(BALL_SIZE, BLUE, pygame.math.Vector2( 1/2 * screen_width, screen_height / 2 ))
+pink_ball   = Ball(BALL_SIZE, PINK, pygame.math.Vector2( 3/4 * screen_width, screen_height / 2 ))
+black_ball  = Ball(BALL_SIZE, BLACK, pygame.math.Vector2( 7/8 * screen_width, screen_height / 2 ))
 # red balls
-red_balls = [Ball(BALL_SIZE, RED, euclid.Vector2(x,y)) for x,y in get_red_ball_positions(screen_width, screen_height, BALL_SIZE, GAP)] 
+red_balls = [Ball(BALL_SIZE, RED, pygame.math.Vector2(x,y)) for x,y in get_red_ball_positions(screen_width, screen_height, BALL_SIZE, GAP)] 
 # creating a list containing all balls
-balls = [green_ball, brown_ball, yellow_ball, blue_ball, black_ball, pink_ball, black_ball]
+balls = [green_ball, brown_ball, yellow_ball, blue_ball, pink_ball, black_ball]
 balls.extend(red_balls)
-white_ball = None
-
+# cue ball
+#cue_ball = Ball(BALL_SIZE, WHITE, pygame.math.Vector2(X_BAULK_D, screen_height / 2), pygame.math.Vector2(100,0))
+#balls.append(cue_ball)
 direction_tick = 0
 
 # Defining variables for fps and running
